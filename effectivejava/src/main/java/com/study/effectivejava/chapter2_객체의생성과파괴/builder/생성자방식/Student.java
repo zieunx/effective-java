57ea1acb3a5e0caefb91a40a212f4ecc99c97d4e
zieunx <wldms_1217@naver.com>
2021-11-01 23:46:27 +0900

    feat(2-2.빌더패턴): 객체의 필수값과 선택값을 생성하는 방식 중 가장 베이직한 생성자 방식으로 구현

---
+++ effectivejava/src/main/java/com/study/effectivejava/chapter2_객체의생성과파괴/builder/생성자방식/Student.java
@@ -0,0 +1,47 @@
+package com.study.effectivejava.chapter2_객체의생성과파괴.builder.생성자방식;
+
+public class Student {
+	private String name; // 필수
+	private String phone; // 필수
+	private int year; // 선택
+	private int month; // 선택
+	private int day; // 선택
+	private float height; // 선택
+	private float weight; // 선택
+
+	public Student(String name, String phone) {
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public Student(String name, String phone, int year) {
+		this.name = name;
+		this.phone = phone;
+		this.year = year;
+	}
+
+	public Student(String name, String phone, int year, int month) {
+		this.name = name;
+		this.phone = phone;
+		this.year = year;
+		this.month = month;
+	}
+
+	public Student(String name, String phone, int year, int month, int day) {
+		this.name = name;
+		this.phone = phone;
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+	public Student(String name, String phone, int year, int month, int day, float height, float weight) {
+		this.name = name;
+		this.phone = phone;
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.height = height;
+		this.weight = weight;
+	}
+}
