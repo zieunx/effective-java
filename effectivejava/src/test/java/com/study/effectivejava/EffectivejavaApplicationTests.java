b5e75a101487b23eaedf1c40be193138865c7308
zieunx <wldms_1217@naver.com>
2021-11-01 23:44:13 +0900

    init project

---
+++ effectivejava/src/test/java/com/study/effectivejava/EffectivejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectivejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
