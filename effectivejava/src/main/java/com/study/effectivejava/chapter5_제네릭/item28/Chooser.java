cc1c70d1bfd672f6f024052da5442668152ee2cc
zieunx <wldms_1217@naver.com>
2022-04-11 01:00:39 +0900

    feat(5-item28/배열보다는 리스트를 사용하라): 제네릭을 쓰지 않고 구현한 Chooser

--- effectivejava/src/main/java/com/study/effectivejava/chapter5_제네릭/item28/Chooser.java
+++ effectivejava/src/main/java/com/study/effectivejava/chapter5_제네릭/item28/Chooser.java
@@ -1,13 +1,19 @@
 package com.study.effectivejava.chapter5_제네릭.item28;
 
+import java.util.Collection;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Chooser {
 	private final Object[] choiceArray;
 
+<<<<<<< HEAD
 	public Chooser(Object[] choiceArray) {
 		this.choiceArray = choiceArray;
+=======
+	public Chooser(Collection choices) {
+		this.choiceArray = choices.toArray();
+>>>>>>> feat(5-item28/배열보다는 리스트를 사용하라): 제네릭을 쓰지 않고 구현한 Chooser
 	}
 
 	public Object choose() {
