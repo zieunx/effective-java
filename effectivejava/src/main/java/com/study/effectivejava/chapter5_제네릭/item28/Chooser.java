fb6d6564b7ab5442a981434803847b26355e6f95
zieunx <wldms_1217@naver.com>
2022-04-11 01:18:21 +0900

    fix(5-item28/배열보다는 리스트를 사용하라): List 기반 Chooser. 'ClassCastException' 이 발생하지 않는다.

--- effectivejava/src/main/java/com/study/effectivejava/chapter5_제네릭/item28/Chooser.java
+++ effectivejava/src/main/java/com/study/effectivejava/chapter5_제네릭/item28/Chooser.java
@@ -1,18 +1,20 @@
 package com.study.effectivejava.chapter5_제네릭.item28;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Chooser<T> {
-	private final T[] choiceArray;
+	private final List<T> choiceList;
 
 	public Chooser(Collection<T> choices) {
-		this.choiceArray = (T[]) choices.toArray();
+		this.choiceList = new ArrayList<>(choices);
 	}
 
 	public Object choose() {
 		Random rnd = ThreadLocalRandom.current();
-		return choiceArray[rnd.nextInt(choiceArray.length)];
+		return choiceList.get(rnd.nextInt(choiceList.size()));
 	}
 }
